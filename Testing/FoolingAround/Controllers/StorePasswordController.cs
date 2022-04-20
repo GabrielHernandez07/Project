470923147b0df6237deb32c8e0e4e7d39731efbf
Gabriel Hernandez <Gabriel.Hernandez-1@ou.edu>
2022-04-19 22:27:02 -0500

    messingaround

---
+++ Testing/FoolingAround/Controllers/StorePasswordController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoolingAround.Controllers
+{
+    public class StorePasswordController : Controller
+    {
+        // GET: StorePassword
+        public ActionResult Password()
+        {
+            string password = passwordText.Value;
+            if(passwordText.Value = 1)
+                {
+
+            }
+            return View();
+        }
+    }
+}
